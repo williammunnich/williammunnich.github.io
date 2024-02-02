23a206b8029bd4f6b42847a2fcd9d6a88fb9e3d6
williammunnich <williammunnich@pm.me>
2024-02-01 20:12:09 -0600

    data dump, format later

--- src/App.tsx
+++ src/App.tsx
@@ -102,6 +102,45 @@ export const App: FC<{ name: string }> = ({ name }) => {
         <a href="https://github.com/williammunnich">William Munnich's GitHub</a>
       </p>
       <CatSprite />
+      <h1>Data Dump (To Sort Later)</h1>
+      <p> 
+EXPERIENCE
+Risk Advisory Intern | Boulay Minneapolis, MN                                                                               June-August 2023
+•	Conducted extensive research on past engagements, and assessed compliance of client companies within the SOC 2 Type 1, SOC 2 Type 2, and Microsoft SSPA frameworks to ensure security adherence
+•	Demonstrated strong analytical abilities and technical skills by meticulously managing evidence, drafting request lists, and utilizing security auditing software like OneTrust and Vanta to bring customer satisfaction and build trust in Boulay
+
+IT Tech Bench Worker & App Developer | SCSU Husky Tech                                                   March 2022-Present    
+•	Provided Microsoft Power Automate knowledge and help to move data reports from the established ticketing system the cloud Aure Database
+•	Collaborated with the SCSU Development Team to build a PowerApps Application and make changes to the ticketing system via API
+•	Resolving 30+ classroom, staff, and student technical and IT issues through the Team Dynamix ticketing system
+•	Reconfiguring 40+ state devices that serve as workstations for the staff and students on campus
+•	Strengthened knowledge of software and physical components of computing by trouble shooting previously unseen issues
+
+Vice-President | SCSU Computer Science Club                                                                                                                August 2023-Present
+President | SCSU Computer Science Club                                                                                                           September 2021-August 2023
+•	Rebuilding Computer Science Club by taking initiative as leader to revive weekly club meetings and Hackathon competitions
+•	Provide students with access to career and skill building activities originating on and off campus
+•	Networking with 4+ alumni and 5+ staff members to bring back dormant club and provide field specific workshops and presentations
+
+Back End Developer | Minneapolis Southwest High School:                                                                                              March-May 2019       
+https://tinyurl.com/wtcc9r87 (GitHub link with code, parts, 3d print models, and pictures)
+•	Collaborated with a team of 6 peers, dividing up tasks like 3d printing, ordering parts, and coding, to create a robot that can be controlled over the network from (almost) any WIFI compatible device.
+•	Learned back-end coding (java and node), using the software development process, to allow for seamless implementation of a predefined goal of building a robot from scratch
+•	Contributed valuable team organization skills by acting as a manager when it was unclear what the next step was to work towards goals and work deal with the constraints of deadlines
+
+PROJECTS 
+ByteMe SCSU Hackathon Fall 2023: Web Scraping for Course Optimization                                         Fall 2023
+•	Developed a cutting-edge web scraping tool using Selenium and Undetected Chrome Driver to assist SCSU students in optimizing their course schedules
+•	Collaborated with team members to design and implement a web scraper that collects course information from SCSU's website
+•	Involved in integrating the Rate My Professor API into the system to enhance the accuracy of course recommendations
+
+eCommerce Website Development for Bouldering Holds                                               June 2023 - August 2023
+•	Successfully developed and launched an eCommerce website for a custom climbing equipment business, showcasing a range of climbing wall holds and volumes
+•	Employed WordPress and WooCommerce, creating an intuitive and user-friendly online store, enhancing the shopping experience as well as the integrated payment and invoicing system
+•	Managed website hosting and backend operations using A2 Hosting and cPanel, ensuring reliable site performance.
+
+•	CERTIFICATIONS: AWS Cloud Practitioner C ertified (May 2023), Vanta Inc. Certified (June 2023), OneTrust Certified (June 2023)
+      </p>
     </div>
   );
 };
